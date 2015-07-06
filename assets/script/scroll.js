9652c8280795eda934f09d854de0282da6eab0fb
Cesar Azevedo <cesardeazevedo@outlook.com>
2015-07-06 04:45:36 -0300

    Update 2015-07-06T07:45:35.210Z

--- assets/script/scroll.js
+++ assets/script/scroll.js
@@ -3,4 +3,34 @@ $(window).scroll(function() {
         $('.head__row').addClass("head__alt");
     else
         $('.head__row').removeClass("head__alt");
+
+});
+
+$(document).ready(function(){
+    var scrollTo = function(to){
+        $.scrollTo(to, 1000, {easing:'easeInOutExpo',offset:0,'axis':'y'});
+    };
+
+    $(".btn--home").click(function(){
+        scrollTo("body");
+    });
+    $(".btn--works").click(function(){
+        scrollTo("#works");
+    });
+
+    $('.btn--platform').click(function(){
+        scrollTo("#platform");
+    });
+
+    $('.btn--worldcup').click(function(){
+        scrollTo('#worldcup');
+    });
+
+    $('.btn--whatis').click(function(){
+        scrollTo('#whatis');
+    });
+
+    $('.btn--beta').click(function(){
+        scrollTo('#beta');
+    });
 });
