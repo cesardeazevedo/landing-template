1920c8567ed641d6384e4c71a543cd1516966c7d
Anderson Juhasc <anderson.juhasc@gmail.com>
2015-07-08 15:01:00 -0300

    Update 2015-07-08T18:01:00.476Z

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
     // portable
-    //enquire.register("screen and (max-width: 1023px)", {
-    //    match : function() {
-    //    }
-    //});
+    enquire.register("screen and (max-width: 1023px)", {
+        match : function() {
+            $('.head-btn').on('click', function(e) {
+                var that = $(this),
+                    nav = $('.head-nav');
+
+                nav.toggleClass('head-nav--open');
+
+                e.preventDefault();
+            });
+        }
+    });
 
     // desktop
     enquire.register("screen and (min-width: 1024px)", {
