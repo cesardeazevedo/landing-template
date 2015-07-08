af14d8835148d7009256b38e6b48adecc4acdc4e
Anderson Juhasc <anderson.juhasc@gmail.com>
2015-07-08 10:58:21 -0300

    Update 2015-07-08T13:57:18.362Z

--- assets/script/scroll.js
+++ assets/script/scroll.js
@@ -1,36 +1,48 @@
 $(window).scroll(function() {
-    if ($(this).scrollTop() > 15)
+    if ($(this).scrollTop() > 15) {
         $('.head__row').addClass("head__alt");
-    else
+    } else {
         $('.head__row').removeClass("head__alt");
-
+    }
 });
 
-$(document).ready(function(){
-    var scrollTo = function(to){
-        $.scrollTo(to, 1000, {easing:'easeInOutExpo',offset:0,'axis':'y'});
+$(document).ready(function() {
+    var scrollTo = function(to) {
+        $.scrollTo(to, 1000, {easing:'easeInOutExpo', offset:0, 'axis':'y'});
     };
 
-    $(".btn--home").click(function(){
+    $(".btn--home").click(function(e){
         scrollTo("body");
+
+        e.preventDefault();
     });
-    $(".btn--works").click(function(){
+    $(".btn--works").click(function(e){
         scrollTo("#works");
+
+        e.preventDefault();
     });
 
-    $('.btn--platform').click(function(){
+    $('.btn--platform').click(function(e){
         scrollTo("#platform");
+
+        e.preventDefault();
     });
 
-    $('.btn--worldcup').click(function(){
+    $('.btn--worldcup').click(function(e){
         scrollTo('#worldcup');
+
+        e.preventDefault();
     });
 
-    $('.btn--whatis').click(function(){
+    $('.btn--whatis').click(function(e){
         scrollTo('#whatis');
+
+        e.preventDefault();
     });
 
-    $('.btn--beta').click(function(){
+    $('.btn--beta').click(function(e){
         scrollTo('#beta');
+
+        e.preventDefault();
     });
 });
