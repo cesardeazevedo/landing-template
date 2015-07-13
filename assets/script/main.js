dbe681e5b94a69159e187bc1118801130f8b363d
cesardeazevedo <cesardeazevedo@outlook.com>
2015-07-12 21:30:30 -0300

    Update 2015-07-13T00:30:30.090Z

--- assets/script/main.js
+++ assets/script/main.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
     // desktop
     enquire.register("screen and (min-width: 1024px)", {
         match : function() {
-            require(["./assets/script/dots.js"]);
+            require(["/assets/script/dots.js"]);
         }
     });
 });
